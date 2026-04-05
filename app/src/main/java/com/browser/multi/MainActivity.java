b4414b238c1c56dfc30cafe73d3725559d101662
ferasdet4-crypto <ferasdet4@gmail.com>
2026-04-05 11:14:08 +0300

    🔥 Ультимативная очистка и заливка проекта

--- app/src/main/java/com/browser/multi/MainActivity.java
+++ app/src/main/java/com/browser/multi/MainActivity.java
@@ -5,21 +5,17 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView wv = new WebView(this);
         setContentView(wv);
-        
         WebSettings s = wv.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
-        
         CookieManager.getInstance().removeAllCookies(null);
         CookieManager.getInstance().flush();
-        
         wv.setWebViewClient(new WebViewClient());
         wv.loadUrl("https://whoer.net");
     }
